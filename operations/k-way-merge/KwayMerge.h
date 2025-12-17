0c3bb01827808ecafc34401bc637316fa1340670
D.Anvari <danialanvari20@gmail.com>
2025-12-17 10:58:27 +0300

    update

--- operations/k-way-merge/KwayMerge.h
+++ operations/k-way-merge/KwayMerge.h
@@ -1,18 +1,60 @@
 #pragma once
 #include <iostream>
-#include <unordered_map>
-#include <string>
-#include <optional>
 #include <vector>
-#include <filesystem>
+#include <algorithm>
+#include <queue>
 
+template<typename T>
+struct MergeElement {
+    T value;
+    size_t block_idx;
+    size_t element_idx;
+
+    bool operator>(const MergeElement& other) const {
+        return value > other.value;
+    }
+};
 
 template<typename T>
 std::vector<T> KwayMergeSort(const std::vector<T>& array
     , std::size_t k = 4
     , std::size_t block_size = 8)
 {
-    /*Todo*/
-    return std::vector<T>();
-}
+    if (array.empty()) return {};
+    if (array.size() <= 1) return array;
 
+    std::vector<std::vector<T>> blocks;
+    for (size_t i = 0; i < array.size(); i += block_size) {
+        size_t end = std::min(i + block_size, array.size());
+        std::vector<T> block(array.begin() + i, array.begin() + end);
+        std::sort(block.begin(), block.end());
+        blocks.push_back(block);
+    }
+
+    std::vector<T> result;
+    result.reserve(array.size());
+    
+    std::priority_queue<MergeElement<T>, std::vector<MergeElement<T>>, std::greater<MergeElement<T>>> pq;
+
+    for (size_t i = 0; i < blocks.size(); ++i) {
+        if (!blocks[i].empty()) {
+            pq.push({blocks[i][0], i, 0});
+        }
+    }
+
+    while (!pq.empty()) {
+        MergeElement<T> current = pq.top();
+        pq.pop();
+        result.push_back(current.value);
+
+        if (current.element_idx + 1 < blocks[current.block_idx].size()) {
+            pq.push({
+                blocks[current.block_idx][current.element_idx + 1],
+                current.block_idx,
+                current.element_idx + 1
+            });
+        }
+    }
+
+    return result;
+}
--- operations/k-way-merge/unit_tests/merge_test.cpp
+++ operations/k-way-merge/unit_tests/merge_test.cpp
@@ -1,19 +1,47 @@
 #include <gtest/gtest.h>
 #include <vector>
+#include <algorithm>
 #include "../KwayMerge.h"
 
 class MergeTest : public ::testing::Test {
 protected:
-    void SetUp() override {
-    }
-
-    void TearDown() override {
-    }
+    void SetUp() override {}
+    void TearDown() override {}
 };
 
-TEST_F(MergeTest, PutAndGet) {
+TEST_F(MergeTest, EmptyArray) {
     std::vector<int> tmp;
     EXPECT_EQ(KwayMergeSort(tmp).size(), 0);
 }
 
-/*TODO*/
+TEST_F(MergeTest, BasicSorting) {
+    std::vector<int> input = {5, 3, 8, 1, 9, 2, 7, 4};
+    std::vector<int> expected = {1, 2, 3, 4, 5, 7, 8, 9};
+    
+    auto result = KwayMergeSort(input, 2, 2);
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, DuplicatesAndNegatives) {
+    std::vector<int> input = {10, -1, 5, 5, -10, 0, 2};
+    std::vector<int> expected = {-10, -1, 0, 2, 5, 5, 10};
+    
+    auto result = KwayMergeSort(input, 3, 3);
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, AlreadySorted) {
+    std::vector<int> input = {1, 2, 3, 4, 5};
+    std::vector<int> expected = {1, 2, 3, 4, 5};
+    
+    auto result = KwayMergeSort(input, 2, 1);
+    EXPECT_EQ(result, expected);
+}
+
+TEST_F(MergeTest, SingleElement) {
+    std::vector<int> input = {42};
+    std::vector<int> expected = {42};
+    
+    auto result = KwayMergeSort(input);
+    EXPECT_EQ(result, expected);
+}
